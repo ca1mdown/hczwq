048c09f571b854b6f9097d8014c3e8572a91e81f
LAPTOP-MQOUQPL3\86134 <707260526@qq.com>
2021-01-09 17:20:49 +0800

    首页菜单选中效果，个人实力:hover

--- src/js/about.js
+++ src/js/about.js
@@ -4,6 +4,10 @@ window.addEventListener('load', function () {
     var jianjie = document.querySelector('.jianjie')
     var chuangbanren = document.querySelector('.chuangbanren')
 
+    var lichengbei_tab = document.querySelector('#lichengbei')
+    var jianjie_tab = document.querySelector('#jianjie')
+    var chuangbanren_tab = document.querySelector('#chuangbanren')
+
     // lichengbei.style.display = 'block'
     console.log(menus.children[0])
 
@@ -11,16 +15,24 @@ window.addEventListener('load', function () {
         lichengbei.style.display = 'block'
         jianjie.style.display = 'none'
         chuangbanren.style.display = 'none'
-
+        lichengbei_tab.classList.add('active')
+        jianjie_tab.classList.remove('active')
+        chuangbanren_tab.classList.remove('active')
     })
     menus.children[1].addEventListener('click', function () {
         lichengbei.style.display = 'none'
         jianjie.style.display = 'block'
         chuangbanren.style.display = 'none'
+        lichengbei_tab.classList.remove('active')
+        jianjie_tab.classList.add('active')
+        chuangbanren_tab.classList.remove('active')
     })
     menus.children[2].addEventListener('click', function () {
         lichengbei.style.display = 'none'
         jianjie.style.display = 'none'
         chuangbanren.style.display = 'block'
+        lichengbei_tab.classList.remove('active')
+        jianjie_tab.classList.remove('active')
+        chuangbanren_tab.classList.add('active')
     })
 })
